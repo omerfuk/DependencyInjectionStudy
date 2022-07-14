bd26417758d89106821be9fbfe87b801250b825a
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-14 23:20:21 +0300

    .

--- DependencyInjectionStudy/ViewController.swift
+++ DependencyInjectionStudy/ViewController.swift
@@ -4,14 +4,32 @@
 //
 //  Created by Ömer Faruk Kılıçaslan on 14.07.2022.
 //
-
+import APIKit
 import UIKit
+import MyAppUIKit
+
+extension APICaller: DataFetchable {}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // https://iosacademy.io/api/v1/courses/index.php
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+        view.addSubview(button)
+        button.backgroundColor = .systemBlue
+        button.setTitle("Tap Me", for: .normal)
+        button.center = view.center
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapButton() {
+        
+        let vc = CoursesViewController(dataFetchable: APICaller.shared)
+        present(vc, animated: true)
+        
     }
 
 
